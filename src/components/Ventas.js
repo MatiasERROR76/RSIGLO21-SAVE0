2cbb183ca73b5c9dd344a94a229b0c32164c9e04
MatiasERROR76 <mat.cistenass@duocuc.cl>
2022-11-10 22:52:22 -0300

    first commit

---
+++ src/components/Ventas.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import HeaderVdiarias from "../template/HeaderVdiarias";
+import Main4 from './Main4';
+
+class Ventas extends Component{
+
+    render(){
+        return(
+           <React.Fragment>
+            <Main4></Main4>
+     <HeaderVdiarias></HeaderVdiarias>
+
+            
+
+     </React.Fragment>
+        );
+    }
+
+}
+
+
+export default Ventas;
